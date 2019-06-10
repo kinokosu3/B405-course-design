8b1fb3211bbb32a9935f841dcfe9ea6760160e67
愉悦の祈 <40928807+kinokosu3@users.noreply.github.com>
2019-06-10 14:09:24 +0800

    update delay a little chance

--- main.c
+++ main.c
@@ -7,12 +7,10 @@
 #define uchar unsigned char
 #define uint unsigned int
 volatile unsigned char sending;
-#define BUFF_MAX 50
+#define BUFF_MAX 30
 unsigned char Buff[BUFF_MAX]; //数据缓冲区
 #define addr1 'a'
 #define addr2 'b'
-#define _SUCC_ 0x0f //数据传送成功
-#define _ERR_ 0xf0  //数据传送失败
 // #define led P1_0
 // #define led1 P1_1
 // #define led2 P1_2
@@ -26,11 +24,12 @@ sbit led1 = P1 ^ 1;
 sbit led2 = P1 ^ 2;
 sbit led3 = P1 ^ 3;
 sbit led4 = P1 ^ 4;
+sbit led5 = P1 ^ 5;
+sbit led6 = P1 ^ 6;
 uchar address_ok, flag;
 uchar status, t0_num = 0, time_out;
 uchar add_flag, address_repeat_flag = 0, rev_data_status, j = 0;
-uchar smod_status, temp_status;
-uchar i = 0;
+uchar i = 0,address_error=0;
 void delay_1ms(unsigned int i)
 {
     unsigned int x, y;
@@ -73,11 +72,7 @@ void Leasts_Sends(char *Buff)
 
 void Clear_Buf(void)
 {
-    unsigned char k;
-    for (k = 0; k < BUFF_MAX; k++) //将缓存内容清零
-    {
-        Buff[k] = 0;
-    }
+    memset(Buff, 0, (BUFF_MAX*sizeof(Buff)));
 }
 
 void main()
@@ -90,20 +85,32 @@ void main()
         add_flag = 1;
         buf = 0xff;
         rev_data_status = 0;
+        led3=1;
+    
         while (add_flag) // 轮询地址
         {
             led1=1;
             led2=1;
+            if(address_repeat_flag != 1 && address_repeat_flag != 0){
+                address_error++;
+                if(address_error == 1){
+                address_repeat_flag = 0;}else if(address_error == 2){
+                    address_repeat_flag=1;
+                    address_error=0;
+                }
+            }
             if (address_repeat_flag == 0)
             {
+                led2=1;
                 TB8 = 1;
                 SBUF = addr1;
                 while (!TI)
                     ;
                 TI = 0;
             }
-            else
+            else if(address_repeat_flag == 1)
             {
+                
                 TB8 = 1;
                 SBUF = addr2;
                 while (!TI)
@@ -120,30 +127,23 @@ void main()
             {
                 led = 1;
             }
-            address_repeat_flag = ~address_repeat_flag;
+            // address_repeat_flag = ~address_repeat_flag;
+            if(address_repeat_flag == 0){address_repeat_flag = 1;}
+            else if(address_repeat_flag == 1){address_repeat_flag = 0;}
         }
         led = 0;
 
-        TI = 0;  //发送数据长度
-        TB8 = 0; //发送数据帧
-        SBUF = 'c';
-        while (!TI)
-            ;
-        TI = 0;
-
         rev_data_status = 1;
-        smod_status = 1;
-        temp_status = 1;
         flag = 0;
-        delay_1ms(300);
+        delay_1ms(500); //500 较稳
 
         if (rev_data_status == 0)
         { //发送数据到上位
-            led4 = 0;
+            led3 = 0;
             if (strstr(Buff, "ppm") != NULL)
-                led3 = 0;
-            // if (strstr(Buff, "11.4C") != NULL)
-            //     led4 = 0;
+                led4 = 0;
+            if (strstr(Buff, "C") != NULL)
+                led5 = 0;
             Leasts_Sends(Buff);
             Clear_Buf();
         }
@@ -166,7 +166,7 @@ void uart() interrupt 4
                     address_ok = 1;
                 }
             }
-            else
+            else if(address_repeat_flag == 1)
             {
                 if (buf == addr2)
                 {
@@ -183,7 +183,7 @@ void uart() interrupt 4
                 flag++;
             }
             i++; //缓存指针向后移动
-            if (flag == 1)
+            if (flag == 2)
             {
                 rev_data_status = 0;
                 i = 0;
